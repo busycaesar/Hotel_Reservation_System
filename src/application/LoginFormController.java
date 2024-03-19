740ec79347f4a5ea000488b1279de647335e5a63
Dev Shah <busycaesar@gmail.com>
2024-03-19 18:50:56 -0400

    Feat(View): Display warning for incorrect id and password

--- src/application/LoginFormController.java
+++ src/application/LoginFormController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 
 public class LoginFormController {
 
@@ -12,13 +13,27 @@ public class LoginFormController {
     private BorderPane root;
     @FXML
     private TextField employeeId, password;
+    @FXML
+    private Text warning;
     private String adminId, adminPassword;
 
     @FXML
     public void initialize() {
+    	
+    	// Get admin credentials
     	AuthenticationController authController = new AuthenticationController();
         this.adminId = authController.getAdminId();
         this.adminPassword = authController.getAdminPassword();
+        
+        // Set defaults
+        this.setDefaults();
+        
+    }
+    
+    private void setDefaults() {
+    	this.employeeId.setText("");
+    	this.password.setText("");
+    	this.warning.setText("");
     }
 
     @FXML
@@ -39,6 +54,8 @@ public class LoginFormController {
         	
         	if (!_receivedEmployeeId.equals(this.adminId) || !_receivedPassword.equals(this.adminPassword)) {
         		System.out.println(_receivedEmployeeId + ", tried logging into the system with wrong credentials!");
+        		this.setDefaults();
+        		this.warning.setText("Incorrect Id/Password");
         		return;
         	}
 
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module hotel_reservation_system {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
