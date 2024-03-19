4f4a0f684a98afe7f6366e0b05c5643a0e1c6fa7
Dev Shah <busycaesar@gmail.com>
2024-03-19 17:42:40 -0400

    Add(Controller): Add reservation controller

---
+++ src/Controller/ReservationController.java
@@ -0,0 +1,19 @@
+package Controller;
+
+import java.util.Date;
+
+import Model.*;
+
+public class ReservationController {
+
+	private Reservation reservation;
+	
+	public ReservationController(String firstName, String lastName, String address, String email, String phone, int totalGuests, Date checkIn, Date checkOut) {
+		
+		Customer customer = new Customer(firstName, lastName, address, email, phone, totalGuests);
+		Room 	 room 	  = new Room();
+		this.reservation  = new Reservation(customer, room, checkIn, checkOut);
+		
+	}
+	
+}
