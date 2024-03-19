bd083b6bafdecd7fb2d4dbc714de3f793abd1a4a
Dev Shah <busycaesar@gmail.com>
2024-03-19 10:49:46 -0400

    Add(Model): Create class for admin credentials

---
+++ src/Model/adminCredentials.java
@@ -0,0 +1,12 @@
+package Model;
+
+public class adminCredentials {
+
+	private long   id = 1234;
+	private String password = "admin";
+	
+	public long getId() { return this.id; }
+	
+	public String getPassword() { return this.password; }
+	
+}
